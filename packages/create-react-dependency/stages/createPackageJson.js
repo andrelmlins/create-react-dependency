b7935142c78ee725102cfe3608345645bbef46ec
André Lins <andrelucas01@hotmail.com>
2019-11-01 10:08:59 -0300

    New Version

--- packages/create-react-dependency/stages/createPackageJson.js
+++ packages/create-react-dependency/stages/createPackageJson.js
@@ -19,7 +19,7 @@ const createPackageJson = (root, name) => {
     devDependencies: {
       react: "16.11.0",
       "react-dom": "16.11.0",
-      "react-dependency-scripts": "0.1.2"
+      "react-dependency-scripts": "0.2.0"
     },
     browserslist: {
       production: [">0.2%", "not dead", "not op_mini all"],
