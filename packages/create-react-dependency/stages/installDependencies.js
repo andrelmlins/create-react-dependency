913c00964eaa33006b2e5e6bb5eae549b316591c
Renato Oda <renato.oda2@gmail.com>
2019-11-01 11:08:26 -0300

    feat: Verifica se o Yarn está instalado

--- packages/create-react-dependency/stages/installDependencies.js
+++ packages/create-react-dependency/stages/installDependencies.js
@@ -2,12 +2,26 @@
 
 const { execSync } = require("child_process");
 
+const yarnInstalled = () => {
+  try {
+    execSync('yarnpkg --version', { stdio: 'ignore' });
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
 const installDependencies = dir => {
   const originalDirectory = process.cwd();
   try {
     process.chdir(dir);
 
-    execSync("yarnpkg install", { stdio: "inherit" });
+    if (yarnInstalled) {
+      execSync("yarnpkg install", { stdio: "inherit" });
+    } else {
+      execSync("npm install", { stdio: "inherit" });
+    }
+
   } catch (err) {
     console.log(`\x1b[31m${err}\x1b[0m`);
   }
