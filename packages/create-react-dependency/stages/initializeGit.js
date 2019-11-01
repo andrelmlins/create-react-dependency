da9b8e42de866270cf671ac5c232cfb44e59cf6b
André Lins <andrelucas01@hotmail.com>
2019-11-01 09:35:13 -0300

    Stage initialize git

---
+++ packages/create-react-dependency/stages/initializeGit.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const { spawnSync } = require("child_process");
+
+const initializeGit = name => {
+  const originalDirectory = process.cwd();
+
+  try {
+    process.chdir(name);
+
+    spawnSync("git", ["init"], { stdio: "inherit" });
+    spawnSync("git", ["add", "*"], { stdio: "inherit" });
+    spawnSync("git", ["commit", "-m", '"First Commit"'], { stdio: "inherit" });
+  } catch (err) {
+    console.log(`\x1b[31m${err}\x1b[0m`);
+  }
+
+  process.chdir(originalDirectory);
+};
+
+module.exports = initializeGit;
