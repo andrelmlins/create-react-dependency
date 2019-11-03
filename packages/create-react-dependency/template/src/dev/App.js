07694189c2153ffc9c5a07df86651b626b2e735c
André Lins <andrelucas01@hotmail.com>
2019-11-03 10:30:15 -0300

    New templates in Create React Dependency

--- packages/create-react-dependency/template/src/dev/App.js
+++ packages/create-react-dependency/template/src/dev/App.js
@@ -1,6 +1,15 @@
 import React from "react";
-import Library from "../lib";
+import Button from "../lib";
+import logo from "./logo.png";
 
-const App = () => <Library />;
+const App = () => (
+  <div className="content">
+    <img src={logo} className="logo" alt="Logo Create React Dependency" />
+    <h1 className="title">Create React Dependency</h1>
+    <section className="library">
+      <Button onClick={() => alert("button")}>My Library Button</Button>
+    </section>
+  </div>
+);
 
 export default App;
--- packages/create-react-dependency/template/src/lib/Lib.js
+++ packages/create-react-dependency/template/src/lib/Lib.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-export const Lib = () => <div>My React Dependency</div>;
+export const Lib = props => (
+  <button
+    style={{
+      border: "none",
+      boxShadow: "none",
+      backgroundColor: "#00838f",
+      color: "#FFF",
+      padding: "0px 20px",
+      fontSize: 20,
+      height: 40
+    }}
+    {...props}
+  />
+);
 
 export default Lib;
