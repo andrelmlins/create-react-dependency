723b1f82241c49dacf60acdd3d6261cc27a7c94f
André Lins <andrelucas01@hotmail.com>
2020-01-22 20:39:50 -0300

    Changing documentation project name

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'create-react-dependency',
   organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
