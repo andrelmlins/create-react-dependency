5e3a9b6d73c3facafd14b9ad7dd8bb94b92d6213
André Lins <andrelucas01@hotmail.com>
2020-01-23 22:47:54 -0300

    Inserting documentation base

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -39,7 +39,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: 'doc1', label: 'Docs' },
+    { doc: 'getting-started', label: 'Docs' },
     { page: 'help', label: 'Help' }
   ],
 
