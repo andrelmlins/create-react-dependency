3272fa71074be18a0ef6efdd410aeb23cc6eaf0e
André Lins <andrelucas01@hotmail.com>
2020-02-28 08:18:08 -0300

    Version 1.0.0

--- packages/create-react-dependency/stages/createPackageJson.js
+++ packages/create-react-dependency/stages/createPackageJson.js
@@ -20,7 +20,7 @@ const createPackageJson = (root, name) => {
     devDependencies: {
       react: '^16.12.0',
       'react-dom': '^16.12.0',
-      'react-dependency-scripts': '^0.6.1'
+      'react-dependency-scripts': '^1.0.0'
     },
     browserslist: {
       production: ['>0.2%', 'not dead', 'not op_mini all'],
