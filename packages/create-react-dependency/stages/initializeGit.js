670c3c56153f199c2361de09799910ddc29ace3d
André Lins <andrelucas01@hotmail.com>
2020-04-02 19:04:25 -0300

    Fix initializeGit

--- packages/create-react-dependency/stages/initializeGit.js
+++ packages/create-react-dependency/stages/initializeGit.js
@@ -10,7 +10,7 @@ const initializeGit = name => {
 
     spawnSync('git', ['init'], { stdio: 'inherit' });
     spawnSync('git', ['add', '*'], { stdio: 'inherit' });
-    spawnSync('git', ['commit', '-m', '"First Commit"'], { stdio: 'inherit' });
+    spawnSync('git', ['commit', '-m', 'First Commit'], { stdio: 'inherit' });
   } catch (err) {
     console.log(`\x1b[31m${err}\x1b[0m`);
   }
