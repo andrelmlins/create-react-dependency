1fa0d23ea626d8523726dcd0e0015ee3864deced
Renato Oda <renato.oda2@gmail.com>
2020-05-12 19:37:29 -0300

    fix: Correção de importação dinamica

--- packages/react-dependency-scripts/configs/babelConfig.js
+++ packages/react-dependency-scripts/configs/babelConfig.js
@@ -1,7 +1,15 @@
 'use strict';
 
 const babelConfigs = {
-  presets: ['@babel/preset-react', '@babel/preset-env'],
+  presets: [
+    '@babel/preset-react',
+    [
+      '@babel/preset-env',
+      {
+        exclude: ['proposal-dynamic-import']
+      }
+    ]
+  ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/proposal-object-rest-spread',
